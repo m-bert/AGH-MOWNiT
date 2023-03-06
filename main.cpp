65c2c24ecdd7810369a5e693e93bdf0ac4ec2120
Michał <misiek.b3870@gmail.com>
2023-03-06 14:21:16 +0100

    Update main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@
 
 void count_bytes(int *float_size_bytes, int *double_size_bytes, int *long_double_size_bytes);
 void count_mantissa_bits(int *float_mantissa_bits, int *double_mantissa_bits, int *long_double_mantissa_bits);
-// void count_exponent_bits(int *float_exponent_bits, int *double_exponent_bits, int *long_double_exponent_bits);
+void print_exponent_bits(int float_exponent_bits, int double_exponent_bits, int long_double_exponent_bits);
+void calculate_epsilons();
+void check_zeros();
 
 int main(int argc, char *argv[])
 {
@@ -16,19 +18,15 @@ int main(int argc, char *argv[])
     count_bytes(&float_size_bytes, &double_size_bytes, &long_double_size_bytes);
     count_mantissa_bits(&float_mantissa_bits, &double_mantissa_bits, &long_double_mantissa_bits);
 
-    // Count exponent bits
-
     int float_exponent_bits = float_size_bytes * BITS_IN_BYTE - float_mantissa_bits;
     int double_exponent_bits = double_size_bytes * BITS_IN_BYTE - double_mantissa_bits;
     int long_double_exponent_bits = long_double_size_bytes * BITS_IN_BYTE - long_double_mantissa_bits;
 
-    std::cout << "====================================" << std::endl;
-    std::cout << "Exponent bits:" << std::endl;
-    std::cout << "float: " << float_exponent_bits << std::endl;
-    std::cout << "double: " << double_exponent_bits << std::endl;
-    std::cout << "long double: " << long_double_exponent_bits << std::endl;
-    std::cout << "====================================" << std::endl;
-    std::cout << std::endl;
+    print_exponent_bits(float_exponent_bits, double_exponent_bits, long_double_exponent_bits);
+
+    calculate_epsilons();
+
+    check_zeros();
 
     return 0;
 }
@@ -78,7 +76,57 @@ void count_mantissa_bits(int *float_mantissa_bits, int *double_mantissa_bits, in
     std::cout << std::endl;
 }
 
-// void count_exponent_bits(int *float_exponent_bits, int *double_exponent_bits, int *long_double_exponent_bits)
-// {
-//     float_exponent_bits =
-// }
+void print_exponent_bits(int float_exponent_bits, int double_exponent_bits, int long_double_exponent_bits)
+{
+    std::cout << "====================================" << std::endl;
+    std::cout << "Exponent bits:" << std::endl;
+    std::cout << "float: " << float_exponent_bits << std::endl;
+    std::cout << "double: " << double_exponent_bits << std::endl;
+    std::cout << "long double: " << long_double_exponent_bits << std::endl;
+    std::cout << "====================================" << std::endl;
+    std::cout << std::endl;
+}
+
+void calculate_epsilons()
+{
+    float float_epsilon = 1.0f, float_tmp_epsilon = 1.0f;
+    while (1.0f + float_tmp_epsilon != 1.0f)
+    {
+        float_epsilon = float_tmp_epsilon;
+        float_tmp_epsilon /= 2.0f;
+    }
+
+    double double_epsilon = 1.0, double_tmp_epsilon = 1.0;
+    while (1.0 + double_tmp_epsilon != 1.0)
+    {
+        double_epsilon = double_tmp_epsilon;
+        double_tmp_epsilon /= 2.0;
+    }
+
+    long double long_double_epsilon = 1.0l, long_double_tmp_epsilon = 1.0l;
+    while (1.0l + long_double_tmp_epsilon != 1.0l)
+    {
+        long_double_epsilon = long_double_tmp_epsilon;
+        long_double_tmp_epsilon /= 2.0l;
+    }
+
+    std::cout << "====================================" << std::endl;
+    std::cout << "Machine epsilon:" << std::endl;
+    std::cout << "float: " << float_epsilon << std::endl;
+    std::cout << "double: " << double_epsilon << std::endl;
+    std::cout << "long double: " << long_double_epsilon << std::endl;
+    std::cout << "====================================" << std::endl;
+    std::cout << std::endl;
+}
+
+void check_zeros()
+{
+    float positive_zero = 0.0f, negative_zero = -0.0f;
+
+    std::cout << "====================================" << std::endl;
+    std::cout << "Zero check:" << std::endl;
+    std::cout << "Positive: " << positive_zero << "  | Sign bit: " << signbit(positive_zero) << std::endl;
+    std::cout << "Positive: " << negative_zero << " | Sign bit: " << signbit(negative_zero) << std::endl;
+    std::cout << "====================================" << std::endl;
+    std::cout << std::endl;
+}
