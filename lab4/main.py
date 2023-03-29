e616a4605ac274639bb8d9f9f76bb24d89911137
Michał <misiek.b3870@gmail.com>
2023-03-29 12:11:40 +0200

    Start lab4

---
+++ lab4/main.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def f(x):
+    return np.sin(2 * x) * np.sin(x**2 / np.pi)
+
+
+def chebyshevKthZero(k, n, a, b):
+    return (1 / 2) * (a + b) + (1 / 2) * (a - b) * np.cos(np.pi * (2 * k - 1) / (2 * n))
+
+
+def chebyshevZeros(n, a, b):
+    return np.array([chebyshevKthZero(x, n, a, b) for x in range(1, n + 1)])
+
+
+def evenSpace(n, a, b):
+    return np.linspace(a, b, n)
+
+
+def getPoints(nodesPositionFn, n, a, b):
+    X = nodesPositionFn(n, a, b)
+
+    P = [(x, f(x)) for x in X]
+
+    return P
