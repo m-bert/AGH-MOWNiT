8cd5e954f720aa8c717d40d9ace781f37287644c
Michał <misiek.b3870@gmail.com>
2023-04-05 11:48:22 +0200

    Add spr and switch names  a->c

--- lab4/main.py
+++ lab4/main.py
@@ -85,7 +85,7 @@ def cubic(x, points, sigmaMatrix):
 # QUADRATIC INTERPOLATION
 
 
-def a(type, i, points):
+def c(type, i, points):
     return (b(type, i+1, points) - b(type, i, points)) / (2*(points[i+1][0] - points[i][0]))
 
 
@@ -101,10 +101,10 @@ def quadratic(x, points, type):
 
     i = min(binarySearch(0, n, x, points), n-2)
 
-    _a = a(type, i, points)
+    _c = c(type, i, points)
     _b = b(type, i, points)
 
-    return _a * np.power((x - points[i][0]), 2) + _b * (x - points[i][0]) + points[i][1]
+    return _c * np.power((x - points[i][0]), 2) + _b * (x - points[i][0]) + points[i][1]
 
 
 # COMMON
@@ -172,4 +172,98 @@ def draw_custom_plot(n, A, B, nodesPosition, interpolation, boundary):
     return
 
 
-draw_custom_plot(10, 0, 3*np.pi, evenSpace, cubic, Boundary.NATURAL)
+def generate_plots(interpolation, boundary):
+    A = 0
+    B = 3 * np.pi
+    T = np.linspace(A, B, 1000)
+    nodes = [4, 5, 7, 10, 15, 20, 30]
+
+    for nodeAmount in nodes:
+        methodName = "F. sklejana"
+        interpolationType = "kwadratowa" if interpolation == quadratic else "sześcienna"
+
+        if boundary == Boundary.CLAMPED:
+            boundaryName = "clamped"
+        elif boundary == Boundary.CUBIC:
+            boundaryName = "cubic"
+        else:
+            boundaryName = "natural"
+
+        points = getPoints(evenSpace, nodeAmount, A, B)
+        interpolationArg = prepareMatrices(
+            points, boundary) if interpolation == cubic else boundary
+
+        plt.title(
+            f"{methodName} {interpolationType} ({boundaryName}) n = {nodeAmount}"
+        )
+        plt.grid()
+        plt.plot(T, I(T, points, interpolation, interpolationArg), color="blue")
+        plt.plot(T, f(T), color="green")
+
+        for point in points:
+            plt.plot(point[0], point[1], marker="o", color="red")
+
+        plt.savefig(
+            f"./screens/{interpolationType}_{boundaryName}_{nodeAmount}.jpg"
+        )
+
+        plt.clf()
+
+    return
+
+
+def generate_all_plots():
+    generate_plots(cubic, Boundary.CUBIC)
+    generate_plots(cubic, Boundary.NATURAL)
+    generate_plots(quadratic, Boundary.NATURAL)
+    generate_plots(quadratic, Boundary.CLAMPED)
+
+    return
+
+
+def get_errors(interpolation, boundary):
+    n = 1000
+
+    A = 0
+    B = 3 * np.pi
+    T = np.linspace(A, B, n)
+
+    nodes = [4, 5, 7, 10, 15, 20, 30]
+
+    for nodeAmount in nodes:
+
+        points = getPoints(evenSpace, nodeAmount, A, B)
+        interpolationArg = prepareMatrices(
+            points, boundary) if interpolation == cubic else boundary
+
+        max_error_fn = np.vectorize(
+            lambda x: np.abs(
+                f(x) - interpolation(x, points, interpolationArg))
+        )
+
+        avg_error_fn = np.vectorize(
+            lambda x: (
+                f(x) - interpolation(x, points, interpolationArg)) ** 2
+        )
+
+        max_error = np.max(max_error_fn(T))
+        avg_error = (np.sqrt(np.sum(avg_error_fn(T)))) / (n-1)
+
+        variant = f"{interpolation.__name__}_{boundary}_{nodeAmount}"
+
+        print(max_error, avg_error, variant)
+
+    return
+
+
+def get_all_errors():
+    get_errors(cubic, Boundary.CUBIC)
+    get_errors(cubic, Boundary.NATURAL)
+    get_errors(quadratic, Boundary.NATURAL)
+    get_errors(quadratic, Boundary.CLAMPED)
+
+    return
+
+
+get_all_errors()
+# draw_custom_plot(3, 0, 3*np.pi, evenSpace, cubic, Boundary.CUBIC)
